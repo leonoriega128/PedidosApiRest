8f53bd7a49d13d60c5588b324cf69c4e437e7436
lnoriega <leonoriega128@gmail.com>
2023-08-31 21:36:20 -0300

    Agregando reglas de validaciones

---
+++ Application/Feautres/Clientes/Commands/CreateContractCommand/CreateContractCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Feautres.Clientes.Commands.CreateContractCommand
+{
+    public class CreateContractCommandValidator : AbstractValidator<CreateContractCommand>
+    {
+        public CreateContractCommandValidator()
+        {
+            RuleFor(p => p.CreatedBy)
+                   .NotEmpty().WithMessage("{PropertyName} no puede ser vacio.")
+                    .MaximumLength(100).WithMessage("{PropertyName} no debe exceder de {MaxLength} caracteres");
+
+            RuleFor(p => p.ContractId)
+                   .NotEmpty().WithMessage("{PropertyName} no puede ser vacio.");
+            RuleFor(p => p.ItemId)
+                 .NotEmpty().WithMessage("{PropertyName} no puede ser vacio.");
+        }
+    }
+}
---
+++ Application/Feautres/Clientes/Commands/CreateCourseCommand/CreateCourseValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Feautres.Clientes.Commands.CreateCourseCommand
+{
+    public class CreateCourseValidator : AbstractValidator<CreateCourseCommand>
+    {
+        public CreateCourseValidator() 
+        {
+            RuleFor(p => p.CourseCode)
+                      .NotEmpty().WithMessage("{PropertyName} no puede ser vacio.")
+                       .MaximumLength(100).WithMessage("{PropertyName} no debe exceder de {MaxLength} caracteres");
+
+            RuleFor(p => p.Commission)
+                   .NotEmpty().WithMessage("{PropertyName} no puede ser vacio.");
+            RuleFor(p => p.State)
+                 .NotEmpty().WithMessage("{PropertyName} no puede ser vacio.");
+        }
+    }
+}
